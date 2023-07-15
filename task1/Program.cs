9a216aa2cae08c275db6118eedca394df1c24ed7
Sancheese95 <sasha_pavlyukovec@mail.ru>
2023-07-15 09:35:02 +0300

    done homework

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введитк число: ");
+int num = int.Parse (Console.ReadLine());
+int newNum = num / 10;
+newNum = newNum % 10;
+Console.WriteLine($"{num} -> {newNum}");
---
+++ task2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введитк число: ");
+int num = int.Parse (Console.ReadLine());
+string anyNum = Convert.ToString(num);
+if(anyNum.Length > 2) Console.WriteLine($" -> {anyNum[2]}");
+else Console.WriteLine($"{num} -> третьей цифры нет");
